b69c1b13adf7e7269ca7aac0d101aa50e26a0dbb
neelchheda <neelchheda45@gmail.com>
2022-01-23 19:13:49 +0530

    FC

---
+++ src/components/nav/header.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+const Header = () => {
+
+    const [state,setstate] = useState('')
+}
+
+
+export default Header;
