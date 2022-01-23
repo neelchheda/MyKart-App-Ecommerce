e72d2c52d49d1da4cf5c95fcf4c95c84340f3732
neelchheda <neelchheda45@gmail.com>
2022-01-23 14:45:12 +0530

    FC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {Switch,Route} from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
 
-import "./App.css";
 
-const App = () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <Switch>
+      <Route path="/" componenent={Home}/>
+    </Switch>
+
+  )
+  };
 
 export default App;
