38e4a823f720ca8d0b39ef8d416b0d52d347893b
neelchheda <neelchheda45@gmail.com>
2022-01-28 15:41:50 +0530

    FC

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,12 +1,27 @@
-import React from "react";
+
+import React,{ useState } from "react";
 
 const Register = () => {
+  const [email,setEmail]=useState('');
+  const handleSubmit=()=>{
+
+  }
+  const registerForm  = () => (
+  <form onSubmit={handleSubmit}>
+      <input 
+      type="email" 
+      className="form-control" 
+      value={email}
+      onChange={(e)=> setEmail(e.target.value)}
+      autoFocus />
+  </form> 
+  );
   return (
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3 mt-10">
           <h4>Register</h4>
-          <p>Register form</p>
+          {registerForm()}
         </div>
       </div>
     </div>
