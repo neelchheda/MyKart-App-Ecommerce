dc53e9e47b4608686f2e95229b35537b7cd8616b
neelchheda <neelchheda45@gmail.com>
2022-02-01 12:47:30 +0530

    extensions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import "antd/dist/antd.css";
+import {createStore} from 'redux';
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
 ReactDOM.render(
