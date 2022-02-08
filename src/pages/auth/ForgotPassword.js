8dde7681fcdc65d9d003deeb8e761a4d8bc017d9
neelchheda <neelchheda45@gmail.com>
2022-02-08 21:36:58 +0530

    forgort password

--- src/pages/auth/ForgotPassword.js
+++ src/pages/auth/ForgotPassword.js
@@ -3,20 +3,36 @@ import { auth } from "../../firebase";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 
-const ForgotPassword = ({hostory})=>{
-    const [email,setEmail]=useState("")
-    const [loading,setLoading]=useState(false)
+const ForgotPassword = ({ hostory }) => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    return <div className="container col-md-6 offset-md-3 p-5">
-        
-        
-        <h4>Forgot Password</h4>
-        <form onSubmit={handleSubmit}>
-          
-        </form>
-        
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    //
 
+  }
+
+  return (
+    <div className="container col-md-6 offset-md-3 p-5">
+      <h4>Forgot Password</h4>
+      <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control mt-3"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Your Email Address"
+        autoFocus
+      />
+      <button className="btn btn-primary mt-3" disabled={!email}>
+        Submit 
+      </button>
+
+
+      </form>
     </div>
-}
+  );
+};
 
 export default ForgotPassword;
