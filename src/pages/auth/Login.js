9737e5c20aa6c822bdb7d7b09ac8a74c46462006
neelchheda <neelchheda45@gmail.com>
2022-02-08 17:38:43 +0530

    Google login

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,44 +2,42 @@ import React, { useState } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
-import { MailOutlined } from "@ant-design/icons";
-import {useDispatch} from 'react-redux';
+import { GoogleOutlined, MailOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
 
-
-
-const Login = ({history}) => {
+const Login = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   let dispatch = useDispatch();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true)
-    try{
-        const result = await auth.signInWithEmailAndPassword(email,password);
-        // console.log(result)
-      const {user} = result;
+    setLoading(true);
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      // console.log(result)
+      const { user } = result;
       const idTokenResult = await user.getIdTokenResult();
       dispatch({
-        type:'LOGGED_IN_USER',
-        payload:{
-          email:user.email,
-          token:idTokenResult,
+        type: "LOGGED_IN_USER",
+        payload: {
+          email: user.email,
+          token: idTokenResult,
         },
       });
-      toast.success(
-        `Login Successfull`
-      );
-      history.push('/');
-    }
-    catch(error){
+      toast.success(`Login Successfull`);
+      history.push("/");
+    } catch (error) {
       console.log(error);
       toast.error(error.message);
       setLoading(false);
     }
-    
   };
+  const googleLogin = () => {
+    //
+  };
+
   const loginForm = () => (
     <form onSubmit={handleSubmit}>
       <input
@@ -79,8 +77,24 @@ const Login = ({history}) => {
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3 mt-10">
-          <h4>Login</h4>
+          {loading ? (
+            <h4>Login</h4>
+          ) : (
+            <h4 className="text-danger">Loading...</h4>
+          )}
           {loginForm()}
+
+          <Button
+            onClick={googleLogin}
+            className="mt-3"
+            type="danger"
+            block
+            shape="round"
+            icon={<GoogleOutlined />}
+            size="large"
+          >
+            Login with Google
+          </Button>
         </div>
       </div>
     </div>
