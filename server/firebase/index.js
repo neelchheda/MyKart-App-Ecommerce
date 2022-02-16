b4294c53bf7e934f14b21c8a66f0861e3f9ff36e
neelchheda <neelchheda45@gmail.com>
2022-02-16 19:21:41 +0530

    firebase amin

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,7 +1,7 @@
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
