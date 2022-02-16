3b58125d3faa62df884befacdcd1a753f55efdc4
neelchheda <neelchheda45@gmail.com>
2022-02-16 17:58:08 +0530

    Server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
-// .connect(process.env.DATABSE,{})
-// .then(()=> console.log("DB CONNECTED"))
-// .catch((err) => console.log("DB ERRORR =>",err))
+
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -14,10 +12,30 @@ const app = express();
 
 // db
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-  })
-  .then(() => console.log("DB CONNECTED"))
-  .catch((err) => console.log("DB CONNECTION ERR", err));
+.connect(process.env.DATABASE,{useNewUrlParser: true,useUnifiedTopology:true})
+.then(()=> console.log("DB CONNECTED"))
+.catch((err) => console.log("DB ERRORR =>",err))
+  // .connect(process.env.DATABASE, {
+    
+  // })
+  // .then(() => console.log("DB CONNECTED"))
+  // .catch((err) => console.log("DB CONNECTION ERR", err));
+
+
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+// route
+app.get("/api", (req, res) => {
+  res.json({
+    data: "hey you hit node API",
+  });
+});
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
