985756bc03a758070d405af63fd2c3734a68aa84
neelchheda <neelchheda45@gmail.com>
2022-02-16 18:12:28 +0530

    orutes middlewares

--- server/server.js
+++ server/server.js
@@ -1,39 +1,33 @@
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+const fs = require("fs"); 
+
+const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
 
 // db
 mongoose
-.connect(process.env.DATABASE,{useNewUrlParser: true,useUnifiedTopology:true})
-.then(()=> console.log("DB CONNECTED"))
-.catch((err) => console.log("DB ERRORR =>",err))
-  // .connect(process.env.DATABASE, {
-    
-  // })
-  // .then(() => console.log("DB CONNECTED"))
-  // .catch((err) => console.log("DB CONNECTION ERR", err));
-
-
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB ERRORR =>", err));
 
 // middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-// route
-app.get("/api", (req, res) => {
-  res.json({
-    data: "hey you hit node API U",
-  });
-});
+//routes middleware
+ app.use('/api' , authRoutes);
+ fs.readdirSync('./routes').map()
 
 // port
 const port = process.env.PORT || 8000;
