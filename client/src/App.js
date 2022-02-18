4ec506c1c7f81f8295c34e4f8a100e0d4e4838d6
neelchheda <neelchheda45@gmail.com>
2022-02-19 00:01:04 +0530

    current user endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,28 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
-import ForgotPassword from "./pages/auth/ForgotPassword"
-import {currentUser} from "./functions/auth";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import { currentUser } from "./functions/auth";
 
-
-import {auth} from './firebase';
+import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
-
 const App = () => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-//to check firebasefromauth
-useEffect(()=>{
-  const unsubscribe= auth.onAuthStateChanged(async(user) =>{
-
-    if(user){
-
-      const idTokenResult = await user.getIdToken();
-      console.log("user",user);
-      createOrUpdateUser(idTokenResult.token)
+  //to check firebasefromauth
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdToken();
+        console.log("user", user);
+        createOrUpdateUser(idTokenResult.token)
           .then((res) => {
             dispatch({
               type: "LOGGED_IN_USER",
@@ -41,15 +35,13 @@ useEffect(()=>{
                 _id: res.data._id,
               },
             });
-            
           })
-          .catch((err)=>console.log(err));
-  //cleanup
-  return()=> unsubscribe();
-
-},[])
-
-
+          .catch((err) => console.log(err));
+      }
+    });
+    //cleanup
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
