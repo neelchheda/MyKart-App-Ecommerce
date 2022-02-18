0b0da32610483923fd62d03822fa5e282a205c09
neelchheda <neelchheda45@gmail.com>
2022-02-18 17:59:58 +0530

    user database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,30 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-      data: "Create or update User API endpoint"
-    });
-  } 
+const User = require("../models/user");
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    {
+      email: email,
+    },
+    {
+      name: name,
+      picture: picture,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (user) {
+    // console.log("USER UPDATED",user)
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      name: name,
+      email: email,
+      picture: picture,
+    }).save();
+    res.json(newUser);
+  }
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,18 @@
-const admin=require("../firebase/index")
+const admin = require("../firebase/index");
 
-exports.authCheck=(req,res,next)=>{
-    console.log(req.headers);           //get the token
-    next(); 
-}
+exports.authCheck = async (req, res, next) => {
+  // console.log(req.headers);           //get the token
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    // console.log("FIREBASE USER IN AUTHCHECK", firebaseUser);
+    req.user = firebaseUser;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      err: "INVALID TOKEN",
+    });
+  }
+ 
+}; 
