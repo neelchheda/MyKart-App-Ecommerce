93349a2ef18df6d6c4350e6df1d166b506661f9d
Neel Chheda <neelchheda45@gmail.com>
2022-02-20 18:13:47 +0530

    Category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,6 +1,20 @@
 const Category = require("../models/category");
+const slugify = require("slugify");
 
-exports.create=()=>{
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await new Category({ name: name, slug: slugify(name) }).save();
+    res.json(category);
+  } catch (err) {
+    onmouseleave.log(err);
+    res.status(400).send("Create Category Failed");
+  }
+};
+exports.read = async (req, res) => {};
 
-}
+exports.update = async (req, res) => {};
 
+exports.list = async (req, res) => {};
+
+exports.remove = async (req, res) => {};
