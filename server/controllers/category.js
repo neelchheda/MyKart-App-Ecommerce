d4afebcef752d1f5269229aa35789a5e23628fed
Neel Chheda <neelchheda45@gmail.com>
2022-02-20 18:51:09 +0530

    Category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -18,10 +18,33 @@ exports.list = async (req, res) => {
   res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
 };
 exports.read = async (req, res) => {
-    let category = await Category.findOne({slug:req.params.slug}).exec();
-    res.json(category);
+  let category = await Category.findOne({ slug: req.params.slug }).exec();
+  res.json(category);
 };
 
-exports.update = async (req, res) => {};
+exports.update = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const update = await Category.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name: req.body.name, slug: slugify(name) },
+      {new:true}
+    );
+    res.json(update)
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Update Category Failed");
+  }
+};
 
-exports.remove = async (req, res) => {};
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Category.findOneAndDelete({
+      slug: req.params.slug,
+    }).exec();
+    req.json(deleted);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Delete Category Failed");
+  }
+};
