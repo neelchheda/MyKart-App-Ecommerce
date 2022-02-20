bfa5b200f74423e0712a86b43771ceb4e049c3ab
Neel Chheda <neelchheda45@gmail.com>
2022-02-20 18:23:47 +0530

    Category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -4,17 +4,24 @@ const slugify = require("slugify");
 exports.create = async (req, res) => {
   try {
     const { name } = req.body;
-    const category = await new Category({ name: name, slug: slugify(name) }).save();
+    const category = await new Category({
+      name: name,
+      slug: slugify(name),
+    }).save();
     res.json(category);
   } catch (err) {
-    onmouseleave.log(err);
+    console.log(err);
     res.status(400).send("Create Category Failed");
   }
 };
-exports.read = async (req, res) => {};
+exports.list = async (req, res) => {
+  res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
+};
+exports.read = async (req, res) => {
+    let category = await Category.findOne({slug:req.params.slug}).exec();
+    res.json(category);
+};
 
 exports.update = async (req, res) => {};
-
-exports.list = async (req, res) => {};
 
 exports.remove = async (req, res) => {};
