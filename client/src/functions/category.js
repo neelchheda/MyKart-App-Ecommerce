8ebb50452b62870b53dd7fe12b551f9d86057f5a
Neel Chheda <neelchheda45@gmail.com>
2022-02-22 18:45:22 +0530

    categories

---
+++ client/src/functions/category.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const getCategory = async () => {
+  return await axios.get(`${process.env.REACT_APP_API}/categories`);
+};
+
+export const getCategory = async (slug) =>  await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
+
+
+export const removeCategory = async (slug,authtoken) => {
+     await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`,
+    {
+    headers: {
+        authtoken:authtoken,
+    }
+}
+);
+export const updateCategory = async (slug,category,authtoken) => 
+    await axios.put(`${process.env.REACT_APP_API}/category/${slug}`,
+   {
+   headers: {
+       authtoken:authtoken,
+   }
+} );
